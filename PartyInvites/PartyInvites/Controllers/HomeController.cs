02a3daf3641c639026c85ddc6eeadd3a7d162ebe
Martin Ivanov Matev <martin.matev89@gmail.com>
2015-03-11 22:26:47 +0200

    Invites /done/

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ViewResult Index()
+        {
+            int currentHour = DateTime.Now.Hour;
+            ViewBag.Greeting = currentHour < 12 ? "Good Morning" : "Good Evening";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RspvForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RspvForm(GuestResponses guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+	}
+}
---
+++ PartyInvites/PartyInvites/Models/GuestResponses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponses
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter whether you will attend the party or not")]
+        public bool? WillAttend { get; set; }
+    }
+}
