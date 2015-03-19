e6cd40cf67e18b581b4c8ded493d9e118c82eb56
Martin Matev <martin.matev89@gmail.com>
2015-03-19 16:19:24 +0200

    Debugging Demo /done/
    Mostly useless, but still...

---
+++ DebuggingDemo/DebuggingDemo/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace DebuggingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int firstVal = 10;
+            int secondVal = 2;
+            int result = firstVal / secondVal;
+
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View(result);
+        }
+    }
+}
