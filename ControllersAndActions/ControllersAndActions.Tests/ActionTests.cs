f914453a1a096868b6b51e4ae70ee90dec7e65e6
Martin Matev <martin.matev89@gmail.com>
2015-03-25 12:38:29 +0200

    Controllers and actions /done/

---
+++ ControllersAndActions/ControllersAndActions.Tests/ActionTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using ControllersAndActions.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControllersAndActions.Tests
+{
+    [TestClass]
+    public class ActionTests
+    {
+        [TestMethod]
+        public void ControllerTest()
+        {
+            // Arrange - create the controller
+            var target = new ExampleController();
+
+            // Act - call the action method
+            RedirectToRouteResult result = target.Redirect();
+
+            // Assert - check the result
+            Assert.IsFalse(result.Permanent);
+            Assert.AreEqual("Example", result.RouteValues["controller"]);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+            Assert.AreEqual("MyID", result.RouteValues["id"]);
+        }
+
+        [TestMethod]
+        public void ViewSelectionTest()
+        {
+            // Arrange - create the controller
+            var target = new ExampleController();
+
+            // Act - call the action method
+            ViewResult result = target.Index();
+
+            // Assert
+            Assert.AreEqual("", result.ViewName);
+            Assert.IsInstanceOfType(result.ViewBag.Date, typeof(System.DateTime));
+        }
+    }
+}
---
+++ ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Sup matey";
+            ViewBag.Date = DateTime.Now;
+            // Take the data from the TempData, deleting it
+            //ViewBag.Message = TempData["Message"];
+            //ViewBag.Date = TempData["Date"];
+
+            // Take it with a peek, leaving it in the TempData container
+            //string message = (string)TempData.Peek("Message");
+            //DateTime date = (DateTime)TempData.Peek("Date");
+
+            // You can preserve a value that would otherwise be deleted by using the Keep method, like this:
+            //TempData.Keep("Message");
+            //TempData.Keep("Date");
+
+            return View();
+        }
+
+        public RedirectToRouteResult Redirect()
+        {
+            return RedirectToRoute(new 
+            {
+                controller = "Example",
+                action = "Index",
+                id = "MyID"
+            });
+        }
+
+        // Assigning temp data
+        public RedirectToRouteResult RedirectToRoute()
+        {
+            TempData["Message"] = "Sup matey";
+            TempData["Date"] = DateTime.Now;
+
+            return RedirectToAction("Index");
+        }
+
+        // you can override the layout used by a view by explicitly naming an alternative, like this:
+        public ViewResult Example()
+        {
+            return View("Index", "_AlternateLayoutPage");
+        }
+
+        public HttpStatusCodeResult StatusCode()
+        {
+            //return HttpNotFound();
+            return new HttpUnauthorizedResult();
+            //return new HttpStatusCodeResult(404, "YOU ARE NOT PREPARED");
+        }
+	}
+}
